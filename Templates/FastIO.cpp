064a7759010da9019fe0c42dda707c6ee08ac782
Dhruv Rajput <raj.dhruv.777@gmail.com>
2021-07-25 15:37:34 +0530

    Adding starter templates

---
+++ Templates/FastIO.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dbl;
+const int mod = 1e9 + 7;
+
+#define v           vector
+#define rep(i,n)    for(int i=0;i<n;++i)
+#define fr(i,s,e)   for(int i=s;i<e;++i)
+#define rfr(i,s,e)  for(int i=s;i>=e;--i)
+#define fi          first
+#define se          second
+#define MP          make_pair
+#define pb 	        push_back
+#define maxint      1e9+1
+#define minint      -1e9-1
+#define all(x)      x.begin(),x.end()
+
+#define scanint(x) scanf("%d",&x)
+#define scanll(x)  scanf("%lld",&x)
+#define scandbl(x) scanf("%lf",&x)
+#define scanull(x) scanf("%llu",&x)
+
+#define printint(x) printf("%d",x)
+#define printll(x) printf("%lld",x)
+#define printdbl(x) printf("%lf",x)
+#define printull(x) printf("%llu",x)
+
+#define gap printf(" ");
+#define line printf("\n")
+
+/*
+use gets and puts for strings.
+use getchar() if something is being scanned before a string.
+*/
+
+void solve() {
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int test_cases = 1;
+	scanf("%d", &test_cases);
+	while (test_cases--) {solve();}
+	return 0;
+}
---
+++ Templates/Starter_template.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dbl;
+const int mod = 1e9 + 7;
+
+#define v           vector
+#define d(x)        cout<<#x<<" = "<<x<<"\n"
+#define d2(x,y)     cout<<#x<<" = "<<x<<"  "<<#y<<" = "<<y<<"\n";
+#define d3(x,y,z)   cout<<#x<<" = "<<x<<"  "<<#y<<" = "<<y<<"  "<<#z <<" = "<<z<<"\n";
+#define d4(x,y,z,a) cout<<#x<<" = "<<x<<"  "<<#y<<" = "<<y<<"  "<<#z <<" = "<<z<<"  "<<#a<<" = "<<a<<"\n";
+#define rep(i,n)    for(int i=0;i<n;++i)
+#define fr(i,s,e)   for(int i=s;i<e;++i)
+#define rfr(i,s,e)  for(int i=s;i>=e;--i)
+#define fi          first
+#define se          second
+#define MP          make_pair
+#define pb 	        push_back
+#define maxint      1e9+1
+#define minint      -1e9-1
+#define test        cout<<"\nTEST_LINE\n"
+#define line        cout<<"\n"
+#define ln          "\n";
+#define all(x)      x.begin(),x.end()
+#define prtvec(x)	cout<<#x<<" : ";for(auto i:x)cout<<i<<' ';line;
+
+
+
+void solve() {
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int test_cases = 1;
+	cin >> test_cases;
+	while (test_cases--) {solve();}
+	return 0;
+}
